eb4ca4dbd148fede26a9ee2541be74c33ac6da84
RSLilova18 <RSLilova18@codingburgas.bg>
2023-02-06 13:20:05 +0200

    Initial commit (polovin zadacha)

---
+++ src/Pharmacy.java
@@ -0,0 +1,19 @@
+public class Pharmacy {
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+
+    public Pharmacy(){
+        this.name="Mareshki";
+        this.address="Sofia";
+        this.phoneNumber="0887637475";
+    }
+
+    public Pharmacy(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+}
